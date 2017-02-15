0cde6ec94554047eb76cb89176d40a259c0186c2
Brandon Elam Barker <beb82@cornell.edu>
2017-02-15 13:50:07 -0500

    adding gist link

--- src/test/scala/edu/cornell/cac/docker/api/JsonComparison.scala
+++ src/test/scala/edu/cornell/cac/docker/api/JsonComparison.scala
@@ -4,6 +4,9 @@ import org.skyscreamer.jsonassert.JSONCompare.compareJSON
 import org.skyscreamer.jsonassert.JSONCompareMode
 /**
   * Created by Brandon on 10/25/2016.
+  *
+  * Add contributions to https://gist.github.com/bbarker/5b0baec6f0c149df245e03c24361d145
+  *
   */
 object JsonComparison {
   def compareJsonLenient(expected: String, actual: String): Boolean =
